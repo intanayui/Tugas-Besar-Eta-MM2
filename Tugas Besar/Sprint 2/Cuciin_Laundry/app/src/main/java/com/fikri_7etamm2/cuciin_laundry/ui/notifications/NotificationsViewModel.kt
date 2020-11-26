2fee89ce43af0528ba9764b63b6ea73e2417417f
MuhammadNurFikri <mf4853432@gmail.com>
2020-11-26 22:52:48 +0700

    Sprint 2 proses pembuatan menu utama

---
+++ Tugas Besar/Sprint 2/Cuciin_Laundry/app/src/main/java/com/fikri_7etamm2/cuciin_laundry/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.fikri_7etamm2.cuciin_laundry.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
